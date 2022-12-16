16d34d7ad13f303aeb9bdeb7257acd0447538749
dnvj <104969047+dnvj@users.noreply.github.com>
2022-12-16 18:56:28 +0200

    LTMH

---
+++ js1/questions1.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Що означає CSS?",
+    answer: "Каскадна таблиця стилів",
+    options: [
+      "Барвиста таблиця стилів",
+      "Комп'ютерна таблиця стилів",
+      "Загальна таблиця стилів",
+      "Каскадна таблиця стилів"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Яка властивість CSS відповідає за заокруглення кутів зовнішньої межі елемента?",
+    answer: "border-radius",
+    options: [
+      "border-size",
+      "border-radius",
+      "border-weight",
+      "border-width"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Яка максимальна одночасна кількість значень може мати властивість border-radius? ",
+    answer: "4",
+    options: [
+      "4",
+      "8",
+      "6",
+      "10"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Яке із цих значень НЕ може бути значенням для font-size?",
+    answer: "4",
+    options: [
+      "50%",
+      "4",
+      "xx-large",
+	  "2 mm"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Як встановити колір шрифту?",
+    answer: "color: red;",
+    options: [
+      "color: red;",
+      "font-color: red;",
+      "font-color: #FF0000;",
+      "font: #FF0000;"
+    ]
+  },
+];
---
+++ js2/questions2.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Де можна використовувати тег script?",
+    answer: "Можна використовувати як у head, так і body",
+    options: [
+      "Можна використовувати як у head, так і body",
+      "Тільки в head",
+      "Тільки в body",
+      "Цей тег не можна ніде використовувати, оскільки він неправильний"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Мова JavaScript є підвидом мови Java – вірно?",
+    answer: "Ні",
+    options: [
+      "Так",
+      "Ні",
+      "Навпаки, Java – підвид JavaScript",
+      "Це все одна мова"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Який з перелічених нижче синтаксис для використання файлу javascript буде правильний? ",
+    answer: "script src='xxx.js'",
+    options: [
+      "script src='xxx.js'",
+      "script href='xxx.js'",
+      "script path='xxx.js'",
+      "script path='xxx.js'"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Який спосіб використання If в javascript правильний",
+    answer: "if(){}",
+    options: [
+      "if []{}",
+      "if(){}",
+      "call if () {}",
+      "(if){}"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Як можна оголосити функцію Javascript?",
+    answer: "function myFunction()",
+    options: [
+      "declare function myFunction()",
+      "myFunction function()",
+      "function myFunction()",
+      "var function myFunction()"
+    ]
+  },
+];
